a63449147cf7632d9a0b224c59c89b55d5892e6c
Shiran8 <shiranamran8@gmail.com>
2023-05-07 12:52:41 +0300

    add new file

---
+++ APRIL 2023/Week 2/exercises.py
@@ -0,0 +1,15 @@
+my_age = 60
+future_age = my_age + 123879
+print(f"I will be {future_age} old in 123879")
+
+amount_miles = int(input("give me a number of miles")) # 10
+amount_km = amount_miles * 1.6
+print(f"{amount_miles} miles is {amount_km} kms")
+
+first_name = "John"
+last_name = "Smith"
+full_name = first_name + " " + last_name
+full_name_two = f"His fullname is {first_name}  - {last_name}" 
+print(full_name)
+print(full_name_two)
+
