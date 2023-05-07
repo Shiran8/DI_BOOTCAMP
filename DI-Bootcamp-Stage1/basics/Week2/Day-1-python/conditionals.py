371e0d10557760259b727ef1e163841bcc5a9469
Shiran8 <shiranamran8@gmail.com>
2023-05-07 15:50:36 +0300

    add file

---
+++ DI-Bootcamp-Stage1/basics/Week2/Day-1-python/conditionals.py
@@ -0,0 +1,18 @@
+# Syntax
+
+# if condition_is_true:
+#     statement
+
+
+bank_amount = 10000
+computer_price = 5000
+phone_price = 2000
+
+if bank_amount >= computer_price :
+    print("i will buy the computer")
+elif bank_amount >= phone_price:
+    print("i will buy the phone")
+
+print("outside the if")
+
+
---
+++ DI-Bootcamp-Stage1/basics/Week2/ExerciseXP/exercisexp.py
@@ -0,0 +1,43 @@
+# Exercise 1
+
+print("Hello world \n" *4)
+
+# Exercise 2
+
+print ("99*3)*8")
+
+# def power(n,e):
+# res=
+# for i in range(e):
+#     res *= n
+#     return res
+# print(power(4,2))
+
+# Excercise 3
+# print(5 < 3) #False
+
+# print( 3 == 3) #True
+
+# print (3 == "3") #False
+
+# print ("3" > 3) #False
+
+# print ("Hello" == "hello") #False
+
+# Exercise 4
+name_brand = "HP"
+print(name_brand)
+print(f"I have a {name_brand} computer")
+
+# Exercise 5
+my_name = "Shiran"
+my_age = 30 
+shoe_size = "39"
+info = "I like dogs"
+print({info})
+
+
+# Exercise 6
+
+
+
